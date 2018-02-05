a993cc02a1a743c8dd9c29c9355e736fd7597abc
AnithaYerra <36034375+AnithaYerra@users.noreply.github.com>
2018-02-05 14:16:12 +0530

    updated full_palidnrome
    just for casual

--- Full_palindrome.java
+++ Full_palindrome.java
@@ -5,11 +5,11 @@ public class Full_palindrome {
 		int i,j,k;
 		for(i=1; i<=4; i++)
 		{
-		for(j=4; j>i; j--)//i added hte comments in this project 
+		for(j=4; j>i; j--)//i edited the  comments in this project 
 		{
 		System.out.print(" ");// printing spaces 
 		}
-		for(k=1; k<=(2*i-1); k++)
+		for(k=1; k<=(2*i-1); k++)//looping 
 		{
 		System.out.print("1");
 		}
